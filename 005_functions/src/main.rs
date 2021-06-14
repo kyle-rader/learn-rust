b5de62b87ae75a22988054119c98ed9beaab077d
Kyle Rader <kylewrader@gmail.com>
2021-06-13 17:49:04 -0700

    005: calling functions

--- 005_functions/src/main.rs
+++ 005_functions/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
+    another_fn(42);
+    let x = 21;
+    println!("{} doubled is {}", x, double(x));
+}
+
+fn another_fn(x: i32) {
+    println!("I'm another_fn. I was given {}", x);
+}
+
+fn double(x: i32) -> i32 {
+    x * 2
 }
