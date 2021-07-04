9f8ef1368f0ce6afa4d5b4b502712656cc9016c9
Kyle Rader <kylewrader@gmail.com>
2021-07-04 12:32:40 -0700

    ownership_move

--- book/04/owner_basic/src/main.rs
+++ book/04/owner_basic/src/main.rs
@@ -7,6 +7,11 @@ fn main() {
         drop(s);
     }
 
-    let s = String::from("Goodbye!!");
-    println!("{}", s);
+    let s1 = String::from("I am string a");
+    println!("s1 is '{}', with length {}", s1, s1.len());
+
+    let mut s2 = s1;
+    s2.push_str("bc");
+    // println!("s1 is '{}', with length {}", s1, s1.len()); // Doesn't work, because s1 is now invalid!
+    println!("s2 is '{}', with length {}", s2, s2.len());
 }
