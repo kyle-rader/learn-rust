3a55ad12445be9a8393a99bf4ec5c7db6bed433b
Kyle Rader <kylewrader@gmail.com>
2021-07-04 16:31:59 -0700

    Rectangle can_hold

--- book/05/rectangles/src/main.rs
+++ book/05/rectangles/src/main.rs
@@ -4,6 +4,16 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn print_area(a: u32) {
     println!("The area of the rectangle is {}.", a);
 }
@@ -24,6 +34,24 @@ fn main() {
 
     print_area(area_struct(&rect1));
     println!("rect1 is {:#?}", rect1);
+
+    print_area(rect1.area());
+
+    let rect2 = Rectangle {
+        width: 80,
+        height: 60,
+    };
+
+    let can_hold = if rect2.can_hold(&rect1) { "can hold" } else { "cannot hold" };
+    println!("{:?} {} {:?}", rect2, can_hold, rect1);
+
+    let rect3 = Rectangle {
+        width: 5,
+        height: 25,
+    };
+
+    let can_hold = if rect3.can_hold(&rect1) { "can hold" } else { "cannot hold" };
+    println!("{:?} {} {:?}", rect3, can_hold, rect1);
 }
 
 fn area(w: u32, h: u32) -> u32 {
