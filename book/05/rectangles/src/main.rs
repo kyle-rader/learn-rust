4b208ebebb8bd81f2fd318c6903e0c312a8456f0
Kyle Rader <kylewrader@gmail.com>
2021-07-04 16:02:20 -0700

    Use tuples over nothing

--- book/05/rectangles/src/main.rs
+++ book/05/rectangles/src/main.rs
@@ -6,8 +6,18 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area(w, h)
     );
+
+    let dims = (30, 50);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_tuple(dims)
+    );
 }
 
 fn area(w: u32, h: u32) -> u32 {
     w * h
+}
+
+fn area_tuple(dims: (u32, u32)) -> u32 {
+    dims.0 * dims.1
 }
