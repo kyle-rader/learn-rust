89deda990f59f87265b539395d4183d8fee8cd50
Kyle Rader <kylewrader@gmail.com>
2021-07-04 15:44:10 -0700

    hello structs

---
+++ book/05/structs/src/main.rs
@@ -0,0 +1,31 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn new_use(email: &str, username: &str) -> User {
+    User {
+        email: String::from(email),
+        username: String::from(username),
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+fn main() {
+    println!("Hello structs");
+
+    let user1 = User {
+        email: String::from("joe@foobar.com"),
+        username: String::from("job_foobar"),
+        active: true,
+        sign_in_count: 2,
+    };
+
+    println!("We have user1 {}", user1.username);
+
+    let user2 = new_use("jane@doe.com", "jane_doe");
+    println!("And user2 {}", user2.username);
+}
