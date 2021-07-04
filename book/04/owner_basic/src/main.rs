3cf2a9321cd7fff0d2344021e74bd5fc3f2c5c80
Kyle Rader <kylewrader@gmail.com>
2021-07-04 12:36:59 -0700

    ownership_clone

--- book/04/owner_basic/src/main.rs
+++ book/04/owner_basic/src/main.rs
@@ -1,3 +1,7 @@
+fn print_string(name: &str, s: &str) {
+    println!("{} is '{}' with length {}", name, s, s.len());
+}
+
 fn main() {
     {
         let mut s = String::from("Hello");
@@ -8,10 +12,14 @@ fn main() {
     }
 
     let s1 = String::from("I am string a");
-    println!("s1 is '{}', with length {}", s1, s1.len());
+    print_string("s1", &s1);
 
     let mut s2 = s1;
     s2.push_str("bc");
     // println!("s1 is '{}', with length {}", s1, s1.len()); // Doesn't work, because s1 is now invalid!
-    println!("s2 is '{}', with length {}", s2, s2.len());
+    print_string("s2", &s2);
+
+    let mut s3 = s2.clone();
+    s3.push_str("defg");
+    print_string("s3", &s3);
 }
