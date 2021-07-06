fd4f77fbc066d53414ca30a36b3d42706ba50e84
Kyle Rader <kylewrader@gmail.com>
2021-07-06 16:47:48 -0700

    Define a library project with some modules

---
+++ book/07/restaurant/src/lib.rs
@@ -0,0 +1,12 @@
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+        fn order_up() {}
+        fn take_payment() {}
+    }
+}
