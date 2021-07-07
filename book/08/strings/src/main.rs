ad43597e8277f257d36191bfadf988883d3c6265
Kyle Rader <kylewrader@gmail.com>
2021-07-07 13:29:09 -0700

    building up strings

--- book/08/strings/src/main.rs
+++ book/08/strings/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let data_str: &str = "Hello, strings 🚀";
+    let data_string = String::from("foo bar baz");
+
+    println!("{}", data_str);
+
+    let mut s: String = data_str.to_string();
+    println!("{}", s);
+
+    s.push_str("💚💨");
+    println!("{}", s);
+    s.push('💤');
+    println!("{}", s);
+
+    let s: String = s + " " + &data_string;
+    //              ^ this s is moved here and no longer valid.
+    //                But we can reclaiom the name in the assignment.
+    println!("{}", s);
+
+    // Better string concats
+    let s2 = format!("{}-{}-{}", data_str, data_string, s);
+    println!("{}", s2);
 }
