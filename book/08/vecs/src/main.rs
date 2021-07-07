99ef84608e9126b8d6d422c320b757d759077c8c
Kyle Rader <kylewrader@gmail.com>
2021-07-07 09:35:00 -0700

    Creating and adding Vec<i32>

--- book/08/vecs/src/main.rs
+++ book/08/vecs/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, vecs!");
+
+    // Now that we are pushing i32's below, we don't need this type annotation.
+    // let mut evens: Vec<i32> = Vec::new();
+    let mut evens: Vec<i32> = Vec::new();
+    let mut odds = vec![1, 3, 5];
+
+    evens.push(0);
+    evens.push(2);
+    evens.push(4);
+
+    odds.push(7);
 }
