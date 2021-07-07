aff6365261165d86fac266cd15c8b97a75f3b1af
Kyle Rader <kylewrader@gmail.com>
2021-07-07 16:34:37 -0700

    practice - calculate average (mean) of input numbers

---
+++ practice/003_averages/src/main.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+use std::io;
+
+fn averages(numbers: &Vec<f64>) -> HashMap<&str, f64> {
+    let mut sum = 0.0;
+    for i in numbers {
+        sum += i;
+    }
+
+    vec![("median", 0.0), ("mean", sum / numbers.len() as f64), ("mode", 0.0)]
+        .iter()
+        .cloned()
+        .collect()
+}
+
+fn main() {
+    println!("Please enter your numbers:");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line!");
+
+    let numbers: Vec<f64> = input
+        // get tokens Iter of &str
+        .split_whitespace()
+        // parse and filter to only those that parse as f64
+        //                 👇 this type annotation isn't required
+        .filter_map(|word: &str| word.parse::<f64>().ok())
+        // filter away NAN, and [NEG]INFITE values
+        .filter(|i| i.is_finite())
+        // collect into Vec<f64>
+        .collect();
+
+    let summary = averages(&numbers);
+    println!("{:#?}", summary);
+}
