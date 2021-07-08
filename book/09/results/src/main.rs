4b85d39689ec4976c1aeab1f8d54d29f1b535443
Kyle Rader <kylewrader@gmail.com>
2021-07-07 19:59:16 -0700

    error handling with file io

--- book/09/results/src/main.rs
+++ book/09/results/src/main.rs
@@ -1,5 +1,32 @@
-use std::fs::File;
-use std::io::{Error, ErrorKind};
+use std::fs::{self, File};
+use std::io::{Error, ErrorKind, Read};
+
+fn read_username_from_file1() -> Result<String, Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file2() -> Result<String, Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::with_capacity(24);
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file3() -> Result<String, Error> {
+    fs::read_to_string("hello.txt")
+}
 
 fn main() {
     println!("Hello, Results!");
@@ -31,10 +58,22 @@ fn main() {
         let f = match File::open(hello_filename) {
             Ok(file) => file,
             Err(error) => match error.kind() {
-                ErrorKind::NotFound => File::create(hello_filename)
-                    .expect(format!("Failed to create {}", hello_filename)),
+                ErrorKind::NotFound => File::create(hello_filename).expect("Failed to create file"),
                 _ => panic!("Problem opening the file {:?}", error),
             },
         };
+    }
+
+    {
+        let username = read_username_from_file3();
+        if let Ok(username) = username {
+            if username.is_empty() {
+                println!("username is empty!");
+            } else {
+                println!("Got username: '{}'", username);
+            }
+        } else if let Err(e) = username {
+            println!("Couldn't get username ({})", e);
+        }
     }
 }
