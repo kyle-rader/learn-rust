33346cba3f082bd6bef534b3f51ac69792b2e05a
Kyle Rader <kylewrader@gmail.com>
2021-07-07 17:21:09 -0700

    Clean up inner match by using .expect shortcut

--- book/09/results/src/main.rs
+++ book/09/results/src/main.rs
@@ -9,19 +9,32 @@ fn main() {
     // Will be an Error, but isn't going to panic.
     let f: Result<File, Error> = File::open(hello_filename);
 
-    let f = match File::open(hello_filename) {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create(hello_filename) {
-                Ok(new_file) => {
-                    println!("Had to create {}", hello_filename);
-                    new_file
+    {
+        let f = match File::open(hello_filename) {
+            Ok(file) => file,
+            Err(error) => match error.kind() {
+                ErrorKind::NotFound => match File::create(hello_filename) {
+                    Ok(new_file) => {
+                        println!("Had to create {}", hello_filename);
+                        new_file
+                    }
+                    Err(e) => panic!("Failed to create {}:{:?}", hello_filename, e),
                 },
-                Err(e) => panic!("Failed to create {}:{:?}", hello_filename, e),
+                _ => {
+                    panic!("Problem opening the file {:?}", error)
+                }
             },
-            _ => {
-                panic!("Problem opening the file {:?}", error)
-            }
-        },
-    };
+        };
+    }
+
+    {
+        let f = match File::open(hello_filename) {
+            Ok(file) => file,
+            Err(error) => match error.kind() {
+                ErrorKind::NotFound => File::create(hello_filename)
+                    .expect(format!("Failed to create {}", hello_filename)),
+                _ => panic!("Problem opening the file {:?}", error),
+            },
+        };
+    }
 }
