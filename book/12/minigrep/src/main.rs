84833b327c9a7ebe87b3ca6aed79c12c8160b404
Kyle Rader <kylewrader@gmail.com>
2021-07-08 16:53:29 -0700

    Get cli args and read file

--- book/12/minigrep/src/main.rs
+++ book/12/minigrep/src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for '{}'", query);
+    println!("In file '{}'", filename);
+
+    let contents = fs::read_to_string(filename).expect("Failed to read the file!");
+
+    println!("Got text:\n{}", contents);
 }
