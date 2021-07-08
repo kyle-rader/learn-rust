d4cc6a2c486771c0cef8ebeb11bdb2585c65ab93
Kyle Rader <kylewrader@gmail.com>
2021-07-08 08:39:26 -0700

    introduce duplicate function only differing in type

--- book/10/largest_function/src/main.rs
+++ book/10/largest_function/src/main.rs
@@ -1,4 +1,4 @@
-fn find_largest(numbers: &[i32]) -> i32 {
+fn largest_i32(numbers: &[i32]) -> i32 {
     let mut largest = numbers[0];
 
     for &n in numbers {
@@ -9,12 +9,28 @@ fn find_largest(numbers: &[i32]) -> i32 {
     largest
 }
 
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 fn main() {
     let numbers = vec![34, 50, 12, 67, 89, 100, 94, 65];
-    let largest = find_largest(&numbers);
+    let largest = largest_i32(&numbers);
     println!("largest number is {}", largest);
 
     let numbers = vec![3102, 34, 6000, 89, 54, 2, 43, 8];
-    let largest = find_largest(&numbers);
+    let largest = largest_i32(&numbers);
     println!("largest number is {}", largest);
+
+    let chars = ['a', 'b', 'c', 'i', 'j', 'k', 'z'];
+    let largest = largest_char(&chars);
+    println!("largest char is {}", largest);
 }
