96027fad2458c35768a8d9ab91a5e6537e28d327
Kyle Rader <kylewrader@gmail.com>
2021-07-08 08:30:14 -0700

    extract function

--- book/10/largest_function/src/main.rs
+++ book/10/largest_function/src/main.rs
@@ -1,25 +1,20 @@
-fn main() {
-    let numbers = vec![34, 50, 12, 67, 89, 100, 94, 65];
-
+fn find_largest(numbers: &Vec<i32>) -> i32 {
     let mut largest = numbers[0];
 
     for n in numbers {
-        if n > largest {
-            largest = n;
+        if *n > largest {
+            largest = *n;
         }
     }
+    largest
+}
 
+fn main() {
+    let numbers = vec![34, 50, 12, 67, 89, 100, 94, 65];
+    let largest = find_largest(&numbers);
     println!("largest number is {}", largest);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-
-    let mut largest = number_list[0];
-
-    for number in number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
-
-    println!("The largest number is {}", largest);
+    let numbers = vec![3102, 34, 6000, 89, 54, 2, 43, 8];
+    let largest = find_largest(&numbers);
+    println!("largest number is {}", largest);
 }
