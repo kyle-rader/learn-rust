2fe4d36159dd78a1304d07274f94063ce6f1b320
Kyle Rader <kylewrader@gmail.com>
2021-07-08 08:54:33 -0700

    Use reduce instead of for loop, and return Option

--- book/10/largest_function/src/main.rs
+++ book/10/largest_function/src/main.rs
@@ -1,36 +1,29 @@
-fn largest_i32(numbers: &[i32]) -> i32 {
-    let mut largest = numbers[0];
-
-    for &n in numbers {
-        if n > largest {
-            largest = n;
-        }
-    }
-    largest
+fn largest_i32(list: &[i32]) -> Option<&i32> {
+    list.iter().reduce(|a, b| if a >= b { a } else { b })
 }
 
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
+fn largest_char(list: &[char]) -> Option<&char> {
+    list.iter().reduce(|a, b| if a >= b { a } else { b })
 }
+
+// Won't work yet as >= can't be used any just any T
+// fn largest<T>(list: &[T]) -> Option<&T> {
+//     list.iter().reduce(|a, b| if a >= b { a } else { b })
+// }
 
 fn main() {
     let numbers = vec![34, 50, 12, 67, 89, 100, 94, 65];
-    let largest = largest_i32(&numbers);
-    println!("largest number is {}", largest);
+    if let Some(largest) = largest_i32(&numbers) {
+        println!("largest number is {}", largest);
+    }
 
     let numbers = vec![3102, 34, 6000, 89, 54, 2, 43, 8];
-    let largest = largest_i32(&numbers);
-    println!("largest number is {}", largest);
+    if let Some(largest) = largest_i32(&numbers) {
+        println!("largest number is {}", largest);
+    }
 
     let chars = ['a', 'b', 'c', 'i', 'j', 'k', 'z'];
-    let largest = largest_char(&chars);
-    println!("largest char is {}", largest);
+    if let Some(largest) = largest_char(&chars) {
+        println!("largest char is {}", largest);
+    }
 }
