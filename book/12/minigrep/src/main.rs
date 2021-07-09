ee77cdabeed5160ffcae78dd8149bc3726435b64
Kyle Rader <kylewrader@gmail.com>
2021-07-08 22:36:22 -0700

    Write to stderr using the eprintln! macro

--- book/12/minigrep/src/main.rs
+++ book/12/minigrep/src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
-        println!("{}", err);
+        eprintln!("{}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Oops, {}", e);
+        eprintln!("Oops, {}", e);
         process::exit(1);
     }
 }
