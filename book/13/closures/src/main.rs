a903cca2e37056f38560aab259345c50103bfb7e
Kyle Rader <kylewrader@gmail.com>
2021-07-08 23:22:34 -0700

    Introduce closure

--- book/13/closures/src/main.rs
+++ book/13/closures/src/main.rs
@@ -1,23 +1,21 @@
 use std::thread;
 use std::time::Duration;
 
-fn sim_calc(i: u32) -> u32 {
-    println!("calculating...");
-    thread::sleep(Duration::from_secs(1));
-    i
-}
-
 fn generate_workout(intensity: u32, random_number: u32) {
-    let result = sim_calc(intensity);
+    let expensive_closure = |num| {
+        println!("calculating...");
+        thread::sleep(Duration::from_secs(1));
+        num
+    };
 
     if intensity < 25 {
-        println!("Today, do {} pushups!", result);
-        println!("Next, do {} situps!", result);
+        println!("Today, do {} pushups!", expensive_closure(intensity));
+        println!("Next, do {} situps!", expensive_closure(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!("Today, run for {} minutes!", result);
+            println!("Today, run for {} minutes!", expensive_closure(intensity));
         }
     }
 }
