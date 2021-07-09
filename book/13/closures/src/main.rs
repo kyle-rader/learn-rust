42ec9a3f4981e95ed42fae805a89eb297e9f6abe
Kyle Rader <kylewrader@gmail.com>
2021-07-08 23:17:30 -0700

    Setup simulated generate workout function

--- book/13/closures/src/main.rs
+++ book/13/closures/src/main.rs
@@ -1,3 +1,39 @@
+use std::thread;
+use std::time::Duration;
+
+fn sim_calc(i: u32) -> u32 {
+    println!("calculating...");
+    thread::sleep(Duration::from_secs(1));
+    i
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            sim_calc(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            sim_calc(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                sim_calc(intensity)
+            );
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, closures!");
+
+    let sim_user_val = 10;
+    let sim_random = 4;
+
+    generate_workout(sim_user_val, sim_random);
 }
