057a6aff1b49c1eee39b79ffa4a908cf81b0d8df
Kyle Rader <kylewrader@gmail.com>
2021-07-08 17:01:17 -0700

    extract cmd args parsing into a function

--- book/12/minigrep/src/main.rs
+++ book/12/minigrep/src/main.rs
@@ -3,8 +3,8 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+
+    let (query, filename) = parse_config(&args);
 
     println!("Searching for '{}'", query);
     println!("In file '{}'", filename);
@@ -12,4 +12,10 @@ fn main() {
     let contents = fs::read_to_string(filename).expect("Failed to read the file!");
 
     println!("Got text:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+    (query, filename)
 }
