fee48d302c4248abe608ee5ef3caa835952f53e2
Kyle Rader <kylewrader@gmail.com>
2021-07-09 00:50:13 -0700

    iterate on vec

--- book/13/iterators/src/main.rs
+++ book/13/iterators/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, iterators!");
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+
+    // for loop takes ownership of iterator so we cannot reuse v1_iter here
+    // assert_eq!(v1_iter.next(), None);
+
+    println!("sum is {}", v1.iter().sum::<i32>());
 }
