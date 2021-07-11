8989dbbf26e5161b240d51e1120acb0bd471527f
Kyle Rader <kylewrader@gmail.com>
2021-07-11 14:23:23 -0700

    we can print a recursive Enum

--- book/15/boxes/src/main.rs
+++ book/15/boxes/src/main.rs
@@ -1,13 +1,15 @@
+#[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
     Nil,
 }
 
-use crate::List::{Cons, Nil};
+use List::{Cons, Nil};
 
 fn main() {
     let b = Box::new(5);
     println!("Hello, box: {}", b);
 
     let l = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("{:?}", l);
 }
