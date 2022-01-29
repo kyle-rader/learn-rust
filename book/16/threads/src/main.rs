ffcf0f72eb51bc3306c212331623294a60cec9eb
Kyle Rader <kylewrader@gmail.com>
2022-01-29 11:23:58 -0800

    Factor out print loop from both threads

--- book/16/threads/src/main.rs
+++ book/16/threads/src/main.rs
@@ -1,25 +1,24 @@
 use std::thread;
 use std::time::Duration;
 
-fn main() {
-    let child_handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("child : {}", i);
-            thread::sleep(Duration::from_millis(5));
-        }
-        println!("child done.🏁");
-    });
-
-    for i in 1..5 {
-        println!("parent: {}", i);
+fn print_n_sleep(name: &str, n: usize) {
+    for i in 1..n {
+        println!("{} : {}", name, i);
         thread::sleep(Duration::from_millis(5));
     }
-    println!("parent done.🏁");
+    println!("{} done.🏁", name);
+}
+
+fn main() {
+    let child_handle = thread::spawn(|| {
+        print_n_sleep("Child", 10);
+    });
+
+    print_n_sleep("Parent", 5);
 
     if let Ok(_) = child_handle.join() {
         println!("Joined child thread!");
-    }
-    else {
+    } else {
         println!("🤮 Failed to join child thread!");
     }
 }
