56c5197cf9fa7af04277c23c21926ba30c069fba
Kyle Rader <kylewrader@gmail.com>
2022-01-29 23:27:52 -0800

    start trait objects

---
+++ book/17/gui/src/lib.rs
@@ -0,0 +1,28 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for comp in self.components.iter() {
+            comp.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // pretend to draw
+        println!("A Button is drawn to the screen!");
+    }
+}
