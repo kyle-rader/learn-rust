4e649c94697ef472d5786b05c3e1aa6e69f043e6
Kyle Rader <kylewrader@gmail.com>
2022-02-15 22:59:38 -0800

    Impl Add with associated Types

--- book/19/traits/src/main.rs
+++ book/19/traits/src/main.rs
@@ -17,6 +17,27 @@ impl Add for Point {
     }
 }
 
+#[derive(Debug)]
+struct Millimeters(u32);
+#[derive(Debug)]
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, rhs: Meters) -> Self::Output {
+        Millimeters(self.0 + rhs.0 * 1000)
+    }
+}
+
+impl Add<Millimeters> for Meters {
+    type Output = Millimeters;
+
+    fn add(self, rhs: Millimeters) -> Self::Output {
+        Millimeters(self.0 * 1000 + rhs.0)
+    }
+}
+
 fn main() {
     println!("Hello, advanced traits!");
 
@@ -24,4 +45,7 @@ fn main() {
     let q = Point { x: 1, y: -2 };
 
     println!("p + q = {:?}", p + q);
+
+    println!("34mm + 2m = {:?}", Millimeters(34) + Meters(2));
+    println!("1m + 255mm = {:?}", Meters(1) + Millimeters(255));
 }
