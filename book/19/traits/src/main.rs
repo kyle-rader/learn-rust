01293144e5c8c230802ad8f42daf3cbdfd12aef7
Kyle Rader <kylewrader@gmail.com>
2022-02-18 00:24:00 -0800

    Improve Add implementations for Meters and Millimeters

--- book/19/traits/src/main.rs
+++ book/19/traits/src/main.rs
@@ -17,24 +17,36 @@ impl Add for Point {
     }
 }
 
-#[derive(Debug)]
-struct Millimeters(u32);
-#[derive(Debug)]
-struct Meters(u32);
+#[derive(Debug, PartialEq)]
+struct Millimeters(f32);
+#[derive(Debug, PartialEq)]
+struct Meters(f32);
+
+impl Add for Millimeters {
+    type Output = Millimeters;
+    fn add(self, rhs: Self) -> Self::Output {
+        Millimeters(self.0 + rhs.0)
+    }
+}
+
+impl Add for Meters {
+    type Output = Meters;
+    fn add(self, rhs: Self) -> Self::Output {
+        Meters(self.0 + rhs.0)
+    }
+}
 
 impl Add<Meters> for Millimeters {
     type Output = Millimeters;
-
     fn add(self, rhs: Meters) -> Self::Output {
-        Millimeters(self.0 + rhs.0 * 1000)
+        Millimeters(self.0 + rhs.0 * 1000.0)
     }
 }
 
 impl Add<Millimeters> for Meters {
-    type Output = Millimeters;
-
+    type Output = Meters;
     fn add(self, rhs: Millimeters) -> Self::Output {
-        Millimeters(self.0 * 1000 + rhs.0)
+        Meters(self.0 + rhs.0 / 1000.0)
     }
 }
 
@@ -46,6 +58,34 @@ fn main() {
 
     println!("p + q = {:?}", p + q);
 
-    println!("34mm + 2m = {:?}", Millimeters(34) + Meters(2));
-    println!("1m + 255mm = {:?}", Meters(1) + Millimeters(255));
+    println!("34mm + 2m = {:?}", Millimeters(34.0) + Meters(2.0));
+    println!("1m + 255mm = {:?}", Meters(1.0) + Millimeters(255.0));
+
+    println!("25m + 75m = {:?}", Meters(25.0) + Meters(75.0));
+    println!("25mm + 75mm = {:?}", Millimeters(25.0) + Millimeters(75.0));
+}
+
+#[cfg(test)]
+mod test {
+    use super::{Meters, Millimeters};
+
+    #[test]
+    fn add_meters() {
+        assert_eq!(Meters(3), Meters(1.0) + Meters(2.0));
+    }
+
+    #[test]
+    fn add_millimeters() {
+        assert_eq!(Millimeters(30.0), Millimeters(10.0) + Millimeters(20.0));
+    }
+
+    #[test]
+    fn add_mm_to_m() {
+        assert_eq!(Meters(5.0) + Millimeters(350.0), Meters(5.35));
+    }
+
+    #[test]
+    fn add_m_to_mm() {
+        assert_eq!(Millimeters(3500.0) + Meters(2.0), Millimeters(5500.0));
+    }
 }
