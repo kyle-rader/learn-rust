24434923638a18a080751b204dcb6caa0aa6641a
Kyle Rader <kylewrader@gmail.com>
2022-02-19 13:48:13 -0800

    Start ch20 web server!

--- book/20/src/main.rs
+++ book/20/src/main.rs
@@ -1,3 +1,26 @@
+use std::net::{TcpListener, TcpStream};
+use std::{io::Read, process};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, servers!");
+
+    let listener = match TcpListener::bind("127.0.0.1:7878") {
+        Ok(bound_port) => bound_port,
+        Err(msg) => {
+            eprintln!("Binding to port 7878 failed!\n{}", msg);
+            process::exit(1);
+        }
+    };
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
