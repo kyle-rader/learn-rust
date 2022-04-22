0ebfa5feda2c72e1e9fffdf334d6fed1a55fe5e0
Kyle Rader <kylewrader@gmail.com>
2022-04-22 01:09:30 -0700

    exercism: anagrams

--- exercism/rust/anagram/src/lib.rs
+++ exercism/rust/anagram/src/lib.rs
@@ -1,9 +1,35 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+type CharMap = HashMap<char, u32>;
+
+fn char_map(word: &str) -> CharMap {
+    let mut map: HashMap<char, u32> = HashMap::new();
+    for c in word.chars() {
+        let count = map.entry(c).or_insert(0);
+        *count += 1;
+    }
+    map
+}
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let mut result: HashSet<&'a str> = HashSet::new();
+    let word_lower = word.to_lowercase();
+    let key = char_map(word_lower.as_str());
+
+    for w in possible_anagrams {
+        if w.len() != word.len() {
+            continue;
+        }
+
+        let w_lower = w.to_lowercase();
+        if w_lower == word_lower {
+            continue;
+        }
+
+        let w_map = char_map(w_lower.as_str());
+        if w_map == key {
+            result.insert(*w);
+        }
+    }
+    result
 }
