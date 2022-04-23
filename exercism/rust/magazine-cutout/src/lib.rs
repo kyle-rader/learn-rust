0e55e151187fd7d7e1ade2a6e835fac7ae6332f2
Kyle Rader <kylewrader@gmail.com>
2022-04-23 13:02:36 -0700

    exercism: magazine-cutout

--- exercism/rust/magazine-cutout/src/lib.rs
+++ exercism/rust/magazine-cutout/src/lib.rs
@@ -5,5 +5,19 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut vocab: HashMap<&str, u32> = HashMap::new();
+    for w in magazine {
+        let entry = vocab.entry(w).or_default();
+        *entry += 1;
+    }
+
+    for w in note {
+        let entry = vocab.entry(w).or_default();
+        if *entry <= 0 {
+            return false;
+        } else {
+            *entry -= 1;
+        }
+    }
+    true
 }
