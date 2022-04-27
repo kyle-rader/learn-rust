b415231fd910891a6c4691820511e9a6b31cb924
Kyle Rader <kylewrader@gmail.com>
2022-04-27 01:20:08 -0700

    Simplify clock to only store minutes

--- exercism/rust/clock/src/lib.rs
+++ exercism/rust/clock/src/lib.rs
@@ -1,40 +1,27 @@
-use std::{f32::MIN, fmt::Display};
+use std::fmt::Display;
 
 #[derive(Debug, PartialEq)]
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
 
-const MINUTES: i32 = 60;
-const HOURS: i32 = 24;
+const HOUR: i32 = 60;
+const DAY: i32 = HOUR * 24;
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let mut hours = hours + minutes / MINUTES;
-
-        let minutes = match minutes % MINUTES {
-            n if n < 0 => {
-                hours -= 1;
-                n + MINUTES
-            }
-            n => n,
-        };
-
-        let hours = match hours % HOURS {
-            n if n < 0 => n + HOURS,
-            n => n,
-        };
-        Clock { hours, minutes }
+        Clock {
+            minutes: (((hours * HOUR + minutes) % DAY) + DAY) % DAY,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock::new(self.hours, self.minutes + minutes)
+        Clock::new(0, self.minutes + minutes)
     }
 }
 
 impl Display for Clock {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+        write!(f, "{:02}:{:02}", self.minutes / HOUR, self.minutes % HOUR)
     }
 }
