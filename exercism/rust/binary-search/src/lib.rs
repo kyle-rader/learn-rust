a2b1079d4f04f7b8a0052b15bba5943d3774c60b
Kyle Rader <kylewrader@gmail.com>
2022-04-29 00:13:33 -0700

    em: binarysearch recursive

--- exercism/rust/binary-search/src/lib.rs
+++ exercism/rust/binary-search/src/lib.rs
@@ -1,7 +1,13 @@
+use std::cmp::Ordering;
+
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+    if array.len() == 0 {
+        return None;
+    }
+    let mid = array.len() / 2;
+    match key.cmp(array.get(mid)?) {
+        Ordering::Equal => Some(mid),
+        Ordering::Less => find(&array[..mid], key),
+        Ordering::Greater => find(&array[mid + 1..], key).map(|i| i + mid + 1),
+    }
 }
