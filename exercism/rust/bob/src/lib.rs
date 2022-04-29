d6235e13465d2726cbdd90ad67d80a4d03e8a7fb
Kyle Rader <kylewrader@gmail.com>
2022-04-28 23:08:19 -0700

    em: bob

--- exercism/rust/bob/src/lib.rs
+++ exercism/rust/bob/src/lib.rs
@@ -1,3 +1,18 @@
+use std::cmp::Ordering;
+
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    let message = message.trim_end();
+    if message.len() == 0 {
+        return "Fine. Be that way!";
+    }
+    let question = message.ends_with('?');
+    let caps = message.chars().any(|c| c.is_ascii_alphabetic())
+        && message.to_ascii_uppercase().cmp(&message.to_string()) == Ordering::Equal;
+
+    match (question, caps) {
+        (true, true) => "Calm down, I know what I'm doing!",
+        (true, false) => "Sure.",
+        (false, true) => "Whoa, chill out!",
+        _ => "Whatever.",
+    }
 }
