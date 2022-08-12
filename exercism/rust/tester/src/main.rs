9f5ef573764bff395f7abf47af5378819e543a4e
Kyle Rader <kylewrader@gmail.com>
2022-08-12 02:28:59 -0700

    Hold problem state in struct, and impl Ord and Display

--- exercism/rust/tester/src/main.rs
+++ exercism/rust/tester/src/main.rs
@@ -4,12 +4,83 @@ use std::{process::{Command, Stdio}, fs, path::{PathBuf, Path}};
 
 use git::git_root;
 
+#[derive(Debug, Ord, Eq, PartialEq, PartialOrd)]
+enum ProblemState {
+    Done,
+    DonePreviously,
+    Todo,
+}
+
+#[derive(Debug, PartialEq, Eq)]
 struct Problem {
     name: String,
     path: PathBuf,
+    state: ProblemState,
 }
 
-fn problems(root: &Path) -> Result<Vec<Problem>, Box<dyn std::error::Error>> {
+impl Ord for Problem {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.state.cmp(&other.state)
+    }
+}
+
+impl PartialOrd for Problem {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match self.state.partial_cmp(&other.state) {
+            Some(core::cmp::Ordering::Equal) => {
+                match self.name.partial_cmp(&other.name) {
+                    Some(core::cmp::Ordering::Equal) => Some(core::cmp::Ordering::Equal),
+                    ord => ord,
+                }
+            }
+            ord => ord,
+        }
+    }
+}
+
+impl std::fmt::Display for Problem {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let emoji = match self.state {
+            ProblemState::Done => "✅",
+            ProblemState::DonePreviously => "🏅",
+            ProblemState::Todo => "❌",
+        };
+        write!(f, "{emoji} {}", self.name)
+    }
+}
+
+impl Problem {
+    fn test(name: String, path: PathBuf) -> Result<Problem, Box<dyn std::error::Error>>  {
+        // caching
+        let snt = path.join("done.snt");
+        if snt.exists() {
+            return Ok(Problem {
+                name,
+                path,
+                state: ProblemState::DonePreviously,
+            });
+        }
+
+        let exit_status = Command::new("cargo")
+            .arg("test")
+            .current_dir(&path)
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .status()?;
+
+        let state = if exit_status.success() {
+            fs::File::create(&snt)?;
+            ProblemState::Done
+        }
+        else {
+            ProblemState::Todo
+        };
+
+        Ok(Problem { name, path, state })
+    }
+}
+
+fn problems(root: &Path) -> Result<Vec<(String, PathBuf)>, Box<dyn std::error::Error>> {
     let mut result = Vec::new();
     for entry in root.read_dir()? {
         let problem = entry?.path();
@@ -21,7 +92,7 @@ fn problems(root: &Path) -> Result<Vec<Problem>, Box<dyn std::error::Error>> {
             .to_str()
             .ok_or("🤔 no problem?")?;
 
-        result.push(Problem { name: String::from(name), path: problem });
+        result.push((String::from(name), problem ));
     }
     Ok(result)
 }
@@ -34,35 +105,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>>  {
     root.push("rust");
     let root = root;
 
-    for problem in problems(&root)? {
-        // caching
-        let snt = problem.path.join("done.snt");
-        if snt.exists() {
-            println!("🏅 {}", problem.name);
-            continue;
+    let mut results: Vec<Problem> = Vec::new();
+    for (name, path) in problems(&root)? {
+        match Problem::test(name.clone(), path) {
+            Ok(problem) => results.push(problem),
+            Err(e) => println!("⚠️ {name} {e}"),
         }
-
-        let name = problem.name;
-
-        match Command::new("cargo")
-            .arg("test")
-            .current_dir(&problem.path)
-            .stdout(Stdio::null())
-            .stderr(Stdio::null())
-            .status() {
-                Ok(exit_status) => {
-                    if exit_status.success() {
-                        println!("✅ {name}");
-                        fs::File::create(&snt)?;
-                    }
-                    else {
-                        println!("🗻 {name}");
-                    }
-                },
-                Err(_) => {
-                    println!("🤔 failed to test {name}?!");
-                },
-            }
     }
+    results.sort();
+
+    for r in results {
+        println!("{r}");
+    }
+
     Ok(())
 }
