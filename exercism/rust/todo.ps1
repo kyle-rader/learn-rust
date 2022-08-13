bc41531c4470dfb49860303045510a25f9bad92f
Kyle Rader <kylewrader@gmail.com>
2022-08-12 18:28:36 -0700

    Update todo.ps1

--- exercism/rust/todo.ps1
+++ exercism/rust/todo.ps1
@@ -1,11 +1,17 @@
-$InformationPreference = "Continue"
+# old Way
+# $InformationPreference = "Continue"
 
-Get-ChildItem ./ | Where-Object { $_.PsIsContainer } | ForEach-Object {
-    Push-Location $_;
-    cargo test 2>&1 > $null;
-    if (!$?) {
-        $problem = $_.Name.Split("\")[-1]
-        Write-Information "Gotta do $problem"
-    }
-    Pop-Location;
-}
+# Get-ChildItem ./ | Where-Object { $_.PsIsContainer } | ForEach-Object {
+#     Push-Location $_;
+#     cargo test 2>&1 > $null;
+#     if (!$?) {
+#         $problem = $_.Name.Split("\")[-1]
+#         Write-Information "Gotta do $problem"
+#     }
+#     Pop-Location;
+# }
+
+# This is The Way
+Push-Location "./tester";
+cargo run
+Pop-Location;
