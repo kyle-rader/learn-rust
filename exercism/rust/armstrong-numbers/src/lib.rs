490e12cd29aacdb66bebd7cff091bca3d0719310
Kyle Rader <kylewrader@gmail.com>
2022-08-13 20:05:59 -0700

    exercism: armstrong-numbers

--- exercism/rust/armstrong-numbers/src/lib.rs
+++ exercism/rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let num_but_stringly = format!("{num}");
+    let power = num_but_stringly.len() as u32;
+
+    let result: u32 = num_but_stringly
+        .chars()
+        .map(|c| (c as u8) - b'0')
+        .map(|n| n as u32)
+        .map(|n| n.pow(power))
+        .sum();
+
+    result == num
 }
