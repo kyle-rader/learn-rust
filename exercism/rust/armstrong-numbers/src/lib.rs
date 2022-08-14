b778c838c06068c23b6ae674ba336c30842584ee
Kyle Rader <kylewrader@gmail.com>
2022-08-14 15:02:32 -0700

    simplify armstrong-numbers

--- exercism/rust/armstrong-numbers/src/lib.rs
+++ exercism/rust/armstrong-numbers/src/lib.rs
@@ -2,12 +2,10 @@ pub fn is_armstrong_number(num: u32) -> bool {
     let num_but_stringly = format!("{num}");
     let power = num_but_stringly.len() as u32;
 
-    let result: u32 = num_but_stringly
+    num_but_stringly
         .chars()
-        .map(|c| (c as u8) - b'0')
-        .map(|n| n as u32)
+        .map(|c| ((c as u8) - b'0') as u32)
         .map(|n| n.pow(power))
-        .sum();
-
-    result == num
+        .sum::<u32>()
+        .eq(&num)
 }
